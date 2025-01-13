449e52f92607bb0f0a2686b487c39876bb43a74d
kpindur <kotaro.pindur@gmail.com>
2025-01-13 18:10:43 +0900

    feat(tree): implement wrapper for TreeGenotype

---
+++ src/tree.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+use pyo3::prelude::*;
+use mycoforge::{operators::sampler::OperatorSampler, tree::core::tree::TreeGenotype};
+
+#[pyclass]
+pub struct PyOperatorSampler {
+    internal: OperatorSampler
+}
+
+#[pyclass]
+pub struct PyTreeGenotype {
+    internal: TreeGenotype
+}
+
+#[pymethods]
+impl PyTreeGenotype {
+    #[new]
+    fn new(arena: Vec<String>, children: HashMap<usize, Vec<usize>>) -> Self {
+        return Self { internal: TreeGenotype::new(arena, children) };
+    }
+
+    #[staticmethod]
+    fn with_arena(arena: Vec<String>) -> Self {
+        return Self { internal: TreeGenotype::with_arena(arena) };
+    }
+
+    fn __str__(&self) -> String {
+        return self.internal.to_string();
+    }
+
+    fn arena(&self) -> Vec<String> {
+        return self.internal.arena().clone();
+    }
+
+    fn children(&self) -> HashMap<usize, Vec<usize>> {
+        return self.internal.children().clone();
+    }
+
+    fn subtree(&self, root: usize) -> usize {
+        return self.internal.subtree(root);
+    }
+
+    fn construct_children(&mut self, sampler: &PyOperatorSampler) -> PyResult<()> {
+        *self.internal.children_mut() = self.internal.construct_children(&sampler.internal);
+        return Ok(());
+    }
+}
