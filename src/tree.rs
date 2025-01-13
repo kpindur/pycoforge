158ff3485022eca34f77b6322012694cf4627c7a
kpindur <kotaro.pindur@gmail.com>
2025-01-13 18:26:00 +0900

    feat(sampler): implement wrappers around OperatorSampler

--- src/tree.rs
+++ src/tree.rs
@@ -8,6 +8,25 @@ pub struct PyOperatorSampler {
     internal: OperatorSampler
 }
 
+#[pymethods]
+impl PyOperatorSampler {
+    #[new]
+    fn new(operators: Vec<String>, arity: Vec<usize>, weights: Vec<f64>) -> Self {
+        return Self { internal: OperatorSampler::new(operators, arity, weights) };
+    }
+
+    fn operators(&self) -> Vec<String> { return self.internal.operators().clone(); }
+
+    fn arities(&self) -> Vec<usize> { return self.internal.arities().clone(); }
+
+    fn weights(&self) -> Vec<f64> { return self.internal.weights().clone(); }
+
+    fn update_weights(&mut self, weights: Vec<f64>) -> PyResult<()> {
+        self.internal.update_weights(weights);
+        return Ok(());
+    }
+}
+
 #[pyclass]
 pub struct PyTreeGenotype {
     internal: TreeGenotype
